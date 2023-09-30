6594b4fa41d22050b223bcdcb0d3e0429465a470
Yassin Ettabaa <yettabaa@e3r9p2.1337.ma>
2023-09-30 01:47:34 +0100

    save diffrent way to read file

--- Cpp9/ex00/main.cpp
+++ Cpp9/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yettabaa <yettabaa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 00:56:10 by yettabaa          #+#    #+#             */
-/*   Updated: 2023/09/29 04:22:22 by yettabaa         ###   ########.fr       */
+/*   Updated: 2023/09/30 01:47:07 by yettabaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,57 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <map>
 
 int main(int ac, char const *av[])
 {
-    (void)ac;
     (void)av;
-    std::ifstream dataFile("data.csv");
-    //prot
-    std::stringstream test;
-    test << dataFile.rdbuf(); //protect rdbuf
-    // std::string test2(test.str());
-    std::string test2;
-    test >> test2;
-    test >> test2;
-    std::cout << test2 <<'\n';
+    try
+    {
+        if (ac != 2)
+            throw std::runtime_error("invalid number of argument!");
+        std::string test2;
+        std::stringstream test;
+        // std::stringstream test3;
+        std::map<std::string, double> dataBase;
+        std::ifstream dataBaseFile("data.csv"); //protect
+        if (!dataBaseFile.is_open())
+            throw std::runtime_error("Error opning Data base file");
+        std::ifstream inputFile(av[1]); //protect
+        if (!inputFile.is_open())
+            throw std::runtime_error("Error opning inpute file");
+        // std::string test2(test.str());
+        // test << dataBaseFile.rdbuf(); //protect rdbuf
+        test >> test2;
+        // int i = 0;
+        while (test)
+        {
+            test >> test2;
+            
+            // (atof(test2.substr(test2.find(',') +1).c_str()) <= 1000)&&(std::cout << test2.substr(0, test2.find(',')) << "   " << test2.substr(test2.find(',') +1) <<'\n');
+            // (std::cout << test2.substr(0, test2.find(',')) << "   " << test2.substr(test2.find(',') +1) <<'\n');
+            dataBase[test2.substr(0, test2.find(','))] = atof(test2.substr(test2.find(',') +1).c_str());
+            // if (i++ == 100)
+            //     break;
+        }
+        // test3 << inputFile.rdbuf(); //protect rdbuf
+        std::getline(inputFile, test2);
+        while (!inputFile.eof())
+        {
+            std::getline(inputFile, test2);
+            // std::cout << test2 <<'\n';
+            
+            // (atof(test2.substr(test2.find(',') +1).c_str()) <= 1000)&&(std::cout << test2.substr(0, test2.find(',')) << "   " << test2.substr(test2.find(',') +1) <<'\n');
+            (std::cout << test2.substr(0, test2.find('|')) << "   " << test2.substr(test2.find('|') +1) <<'\n');
+            // dataBase[test2.substr(0, test2.find(','))] = atof(test2.substr(test2.find(',') +1).c_str());
+            // if (i++ == 100)
+            //     break;
+        }
+        
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
     return 0;
 }
--- Cpp9/testcpp9.cpp
+++ Cpp9/testcpp9.cpp
@@ -6,9 +6,18 @@
 int main()
 {
     std::map<std::string, double> map;
+    std::map<std::string, double>::iterator it;
+    
 
+    map["test"] = 48;
     map["test"] = 45;
+    map["test"] = 41;
     map["test1"] = 4;
+
+    // std::cout << map["test"] <<'\n';
+    // std::cout << map["test"] <<'\n';
+    for (it = map.begin(); it != map.end(); it++)
+        std::cout << it->first << ' ' << it->second<<'\n';
 
     
 }
